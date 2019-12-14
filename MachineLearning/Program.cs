94e0f2d350464789f076ef5c275ac508b7fc2e0d
unknown <chasewilliam1999@gmail.com>
2019-12-14 12:28:28 -0500

    Finished basic tutorial

---
+++ MachineLearning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MachineLearningML.Model;
+
+namespace MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput()
+            {
+                SentimentText = "That is Rude."
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+            Console.ReadLine();
+        }
+    }
+}
